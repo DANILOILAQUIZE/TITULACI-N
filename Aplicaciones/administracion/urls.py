65cf2f1803a72a64762849c9b0d74832d9056586
DANILOILAQUIZE <dilaquitze@gmail.com>
2025-05-13 22:31:47 -0500

    COMIT SEMANA6.1.1

--- Aplicaciones/administracion/urls.py
+++ Aplicaciones/administracion/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-
+    path('', views.index, name='index'),
     path('', views.dashboard, name='dashboard'),
     path('administracion/plantilla', views.plantilla, name='administracion/plantilla'),
     
--- Aplicaciones/administracion/views.py
+++ Aplicaciones/administracion/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
 
 def dashboard(request):
     return render(request, 'rol/dashboard.html')
